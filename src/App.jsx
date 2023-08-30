a1214946673eed3854f584b841b6d6189f2a20a6
deja232 <84335792+deja232@users.noreply.github.com>
2023-08-30 11:11:47 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import './App.css'
+
+const modules = {
+  toolbar:[
+    [{font:[]}], 
+    [{size :[]}],
+    ["bold","italic","underline","strike","blockquote"],
+    [{ 'align': [] }],
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'indent': '-1'}, { 'indent': '+1' }],
+    [{ 'color': [] }, { 'background': [] }],    
+    ["link","image"],    
+
+    ],
+  };
+
+const App = () => {
+  const [value, setValue] = useState("")
+
+  return (
+    <>
+ <div className ="container">
+  <div className="editor">  </div>
+    <div className="editable"> </div>
+      <ReactQuill
+      theme ="snow"
+      value={value}
+      onChange ={setValue}
+      className="input"
+      modules={modules}
+      />
+    </div>
+    </>
+  )
+}
+
+export default App
